99d12e5f97fcb6ff214cd61e9d80dbfba8ca245f
veer <Kadvanejanvi@gmail.com>
2024-07-24 19:01:30 +0530

    mongoDB connected

--- banckEnd/server.js
+++ banckEnd/server.js
@@ -1,6 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 dotenv.config()
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("connect to mongoDB");
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express()
 
